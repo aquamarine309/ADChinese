17512baf001e5ddddc08a7a3d596839e48308a65
aquamarine <aquamarine3090@gmail.com>
2025-11-01 18:48:01 +0800

    fix

--- src/components/modals/challenges/NormalChallengeStartModal.js
+++ src/components/modals/challenges/NormalChallengeStartModal.js
@@ -19,13 +19,13 @@ export default {
       return this.challenge.isCompleted;
     },
     message() {
-      return `你将执行大坍缩（如果可能）并在挑战中开启新的无限进程，同时所有挑战特殊限制与改动效果生效。为完成该挑战${this.challengeIsCompleted ? "" : "并获得奖励"}，你必须达成 ${format(NormalChallenge(this.id).goal)} 反物质的挑战目标。无论升级如何，你都不会以任何维度提升或星系开始。`;
+      return `你将执行大坍缩（如果可能）并在挑战中开启新的无限，同时所有挑战特殊限制与改动效果生效。为完成该挑战${this.challengeIsCompleted ? "" : "并获得奖励"}，你必须达成 ${format(NormalChallenge(this.id).goal)} 反物质的挑战目标。无论升级如何，你都不会以任何维度提升或星系开始。`;
     },
     entranceLabel() {
       return `你将要进入挑战 ${this.id}`;
     },
     reward() {
-      return `完成该挑战的奖励为：${rewardDescription}`;
+      return `完成该挑战的奖励为：${this.challenge.rewardDescription}`;
     },
     condition() {
       let conditionOfChallenge = this.challenge._config.description;
